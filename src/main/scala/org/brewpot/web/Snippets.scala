455160ca39883ed7608c25c81745967b65b63e82
Kåre Blakstad <kare.blakstad@finn.no>
2012-11-08 12:11:27 +0100

    bootstrap is now responsive

--- src/main/scala/org/brewpot/web/Snippets.scala
+++ src/main/scala/org/brewpot/web/Snippets.scala
@@ -37,8 +37,8 @@ object Snippets {
             {title}
           </title>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-          <link href="/css/bootstrap-responsive.css" rel="stylesheet"/>
           <link href="/css/bootstrap.min.css" rel="stylesheet"/>
+          <link href="/css/bootstrap-responsive.min.css" rel="stylesheet"/>
           <link href="/css/brewpot.css" rel="stylesheet"/>
         </head>
         <body>

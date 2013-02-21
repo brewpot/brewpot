5530c52177087c69a3ef33ca5ae5f87370fd8c9c
Kåre Blakstad <kare.blakstad@finn.no>
2013-02-21 08:54:14 +0100

    html tjafs

--- src/main/scala/org/brewpot/Bootstrap.scala
+++ src/main/scala/org/brewpot/Bootstrap.scala
@@ -11,12 +11,13 @@ object Bootstrap {
         </title>
         <meta name="viewport" content="width=device-width; initial-scale=1.0;"/>
         <link href="/css/bootstrap.min.css" rel="stylesheet"/>
-        <link href="/css/bootstrap-responsive.min.css" rel="stylesheet"/>
         <link href="/css/brewpot.css" rel="stylesheet"/>
+        <link href="/css/bootstrap-responsive.min.css" rel="stylesheet"/>
       </head>
       <body>
         <div class="navbar navbar-static-top" data-toggle="collapse" data-target=".nav-collapse">
           <div class="navbar-inner">
+            <div class="container">
             <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
@@ -25,27 +26,27 @@ object Bootstrap {
             <a class="brand" href="/">
               Brewpot!
             </a>
-            <div class="nav-collapse collapse">
-              <ul class="nav">
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    Calculators <b class="caret"></b>
-                  </a>
-                  <ul class="dropdown-menu" data-no-collapse="true">
-                    <li>
-                      <a href="/calc/abv"><b>ABV</b> Alcohol by volume</a>
-                    </li>
-                    <li>
-                      <a href="/calc/og"><b>OG</b> Original gravity</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-
+              <div class="nav-collapse collapse">
+                <ul class="nav">
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                      Calculators <b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu" data-no-collapse="true">
+                      <li>
+                        <a href="/calc/abv"><b>ABV</b> Alcohol by volume</a>
+                      </li>
+                      <li>
+                        <a href="/calc/og"><b>OG</b> Original gravity</a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-        <div class="container">
+        <div class="container container-top">
           {content}
         </div>
         <footer class="footer">
